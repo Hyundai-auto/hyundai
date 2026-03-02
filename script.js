3ab7556e80723f545d3cc598823a7cfad5663b8c
Hyundai-auto <comercial12.bra@gmail.com>
2026-03-01 21:43:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Configurações globais
 const CONFIG = {
-    whatsappNumber: '5511946358091',
+    whatsappNumber: '5511967439577',
     whatsappBaseUrl: 'https://wa.me/',
     businessHours: {
         start: 8,
